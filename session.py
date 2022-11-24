a1604cfe49f4b07d60dc2b7cbce004518bd95c25
Cissa Kind <cissakind@gmail.com>
2022-11-23 21:16:50 -0300

    added transaction methods and tests

--- session.py
+++ session.py
@@ -1,2 +1,96 @@
-def add_numbers(a,b):
-	return a+b
+class transaction:
+	def __init__(self, merchant, transaction_time, transaction_amount):
+		self.merchant = merchant
+		self.transaction_time = transaction_time
+		self.transaction_amount = transaction_amount
+
+class account:
+	def __init__(self, active, availableLimit):
+		self.active = active
+		self.availableLimit = availableLimit
+		self.newAccount = True
+		self.history = []
+
+	def isAccountActive(self):
+		return self.active
+
+	def first_transaction_above_threshold(self, transaction_value):
+		if self.newAccount:
+			if transaction_value > self.availableLimit*0.9:
+				return True
+		return False
+
+	def insufficient_limit(self, transaction_value):
+		return (transaction_value >  self.availableLimit)
+
+	def doubled_transaction(self, newTransaction):
+		transaction_size = len(self.history)
+
+		if transaction_size >= 1:
+			for transaction in self.history:
+				if (newTransaction.merchant == transaction.merchant and
+				    newTransaction.transaction_time - transaction.transaction_time < 120):
+					return True
+		return False
+
+	def high_frequency_small_interval(self, newTransaction):
+		transaction_size = len(self.history)
+
+		if(transaction_size >= 2):
+			return ( newTransaction.transaction_time - self.history[transaction_size - 2].transaction_time) < 180
+
+		return False
+
+
+	def updateBalance(self, transaction_value):
+		self.availableLimit = self.availableLimit - transaction_value
+
+	def addTransaction(self, newTransaction):
+		self.newAccount = False
+		self.history += [newTransaction]
+
+
+def autorizador(newTransaction, account):
+	violations = []
+	resultAccount = None
+
+	if(not account.isAccountActive()):
+	
+		resultAccount = account
+		violations += ["account-not-active"]
+		return resultAccount, violations
+	
+
+	elif (account.first_transaction_above_threshold(newTransaction.transaction_amount)):	
+		
+		resultAccount = account;
+		violations += ["first-transaction-above-threshold"]
+		return resultAccount, violations
+	
+
+	elif (account.insufficient_limit(newTransaction.transaction_amount)):
+	
+		resultAccount = account;
+		violations += ["insufficient-limit"]
+		return resultAccount, violations
+	
+
+	elif (account.high_frequency_small_interval(newTransaction)):
+	
+		resultAccount = account;
+		violations += ["high_frequency_small_interval"]
+		return resultAccount, violations
+	
+
+	elif (account.doubled_transaction(newTransaction)):
+	
+		resultAccount = account;
+		violations += ["doubled-transaction"]
+		return resultAccount, violations
+	
+
+	account.updateBalance(newTransaction.transaction_amount)
+	account.addTransaction(newTransaction)
+	resultAccount = account
+	return resultAccount, violations
+
--- test_session.py
+++ test_session.py
@@ -1,7 +1,52 @@
 #import pytest
-import pytest
+from session import *
 
-from session import add_numbers
+def test_high_frequency_small_interval():
+	t1 = transaction('paris6', 0, 10)
+	a1 = account(True, 100.0)
+	resultAccount, violations = autorizador(t1, a1)
 
-def test_add_positive():
-	assert add_numbers(1,2) == 3
+	t2 = transaction('paris7', 10, 1)
+	resultAccount2, violations2 = autorizador(t2, a1)
+
+	t3 = transaction('paris8', 30, 1)
+	resultAccount3, violations3 = autorizador(t3, a1)
+
+	assert(resultAccount3.availableLimit == 89)
+	assert(violations3 == ['high_frequency_small_interval'])
+
+def test_doubled_transaction():
+	t1 = transaction('paris6', 300, 1)
+	a1 = account(True, 100.0)
+	resultAccount, violations = autorizador(t1, a1)
+
+	t2 = transaction('paris6', 400, 1)
+	resultAccount2, violations2 = autorizador(t2, a1)
+
+	assert(resultAccount2.availableLimit == 99)
+	assert(violations2 == ['doubled-transaction'])
+
+def test_new_account_threshold():
+	a1 = account(True, 100.0)
+	t1 = transaction('paris5', 0, 91)
+	resultAccount, violations = autorizador(t1, a1)
+
+	assert(resultAccount.availableLimit == 100)
+	assert(violations == ['first-transaction-above-threshold'])
+
+def test_insufficient_limit():
+	a1 = account(True, 100.0)
+	t1 = transaction('paris5', 400, 10)
+	autorizador(t1, a1)
+	t2 = transaction('paris5', 800, 100)
+	resultAccount, violations = autorizador(t2, a1)
+
+	assert(resultAccount.availableLimit == 90)
+	assert(violations == ['insufficient-limit'])
+
+
+def test_not_active():
+	a1 = account(False, 100.0)
+	t1 = transaction('paris5', 0, 10)
+	resultAccount, violations = autorizador(t1, a1)
+	assert(violations == ['account-not-active'])
